c1cea335cd56aec76e7e8205db9219cc904494f3
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-03 19:39:30 +0000

    add default value to user private column

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@
 #  encrypted_password     :string           default(""), not null
 #  likes_count            :integer          default(0)
 #  photos_count           :integer          default(0)
-#  private                :boolean
+#  private                :boolean          default(TRUE)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   has_many :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id"
   has_many :accepted_sent_follow_requests, -> { where( status: "accepted" ) }, class_name: "FollowRequest", foreign_key: "sender_id"
   has_many :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id"
-  has_many :accepted_received_follow_requests, -> { where(status: "accepted") } class_name: "FollowRequest", foreign_key: "recipient_id"
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, class_name: "FollowRequest", foreign_key: "recipient_id"
   has_many :likes, class_name: "User", foreign_key: "fan_id"
   has_many :liked_photos, through: :likes, source: :photo
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
