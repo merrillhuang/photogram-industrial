54c6b55584a2b442d8c88bd976f1c720d532fcfa
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-03 21:33:35 +0000

    add /username/liked route

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "/:username" => "users#show", as: :user
+  get "/:username/liked" => "users#liked", as: :liked
 end
