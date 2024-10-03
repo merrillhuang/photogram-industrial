42271b08ce294ed814ede883c3ec50d965d4dd8e
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-03 22:19:00 +0000

    define methods for routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
-  before_action :get_user, only: [:show, :liked]
+  before_action :get_user, only: [:show, :liked, :feed, :discover, :followers, :following]
 
   def show
   end
 
   def liked
+  end
+
+  def feed
+  end
+
+  def discover
+  end
+
+  def follwers
+  end
+
+  def following
   end
 
   private
