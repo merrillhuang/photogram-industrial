94646a01f2e8860f2a26ddf45c2815e2de16758d
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-04 14:22:01 +0000

    move fetching of user's photos/followers/etc from template to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,27 @@ class UsersController < ApplicationController
   before_action :get_user, only: [:show, :liked, :feed, :discover, :followers, :following]
 
   def show
+    @own_photos = @user.own_photos
   end
 
   def liked
+    @liked_photos = @user.liked_photos
   end
 
   def feed
+    @feed = @user.feed
   end
 
   def discover
+    @discover = @user.discover
   end
 
   def follwers
+    @followers = @user.followers
   end
 
   def following
+    @following = @user.following
   end
 
   private
